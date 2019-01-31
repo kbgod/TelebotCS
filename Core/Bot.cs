2e7f95bdde5fdb5cadee768f2b30ce333dd29492
Askold Monarkhov <askold.monarkhov@gmail.com>
2019-02-01 00:06:00 +0200

    Revert "Revert "Initial commit""
    This reverts commit cfda462b54910209096c991cd1e3fcf665240baa.

---
+++ Core/Bot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelebotCS.Core
+{
+    public class Bot
+    {
+        private string token;
+        public Bot(string token)
+        {
+            this.token = token;
+        }
+
+        public void GetToken()
+        {
+            Console.WriteLine(this.token);
+        }
+    }
+}
